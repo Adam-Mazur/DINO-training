3adb687345359365aecb22f8257f2dd2b5e1357b
Adam Mazur <adammazur445@gmail.com>
2026-01-28 12:58:14 +0100

    feat(train): Add support for mixed precision

--- src/train.py
+++ src/train.py
@@ -38,31 +38,6 @@ def main(cfg: DictConfig):
         drop_last=True,
     )
 
-    model = DINOModel(
-        output_dim=cfg.model.architecture.output_dim,
-        use_bn_in_head=cfg.model.architecture.use_bn_in_head,
-        norm_last_layer=cfg.model.architecture.norm_last_layer,
-        model_name=cfg.model.architecture.model_name,
-        patch_size=cfg.model.architecture.patch_size,
-        drop_path_rate=cfg.model.architecture.drop_path_rate,
-        lr=cfg.model.train.lr,
-        min_lr=cfg.model.train.min_lr,
-        batch_size_per_gpu=cfg.model.train.batch_size_per_gpu,
-        warmup_epochs=cfg.model.train.warmup_epochs,
-        weight_decay=cfg.model.train.weight_decay,
-        weight_decay_end=cfg.model.train.weight_decay_end,
-        warmup_teacher_temp=cfg.model.train.warmup_teacher_temp,
-        teacher_temp=cfg.model.train.teacher_temp,
-        warmup_teacher_temp_epochs=cfg.model.train.warmup_teacher_temp_epochs,
-        student_temp=cfg.model.train.student_temp,
-        center_momentum=cfg.model.train.center_momentum,
-        local_crops_number=cfg.model.transform.local_crops_number,
-        momentum_teacher=cfg.model.train.momentum_teacher,
-        world_size=cfg.machine.num_gpus * cfg.machine.num_nodes,
-        n_epochs=cfg.model.train.n_epochs,
-        n_dataloader_steps=len(dataloader),
-    )
-
     checkpoint_cb = ModelCheckpoint(
         dirpath=run_ckpt_dir,
         filename="epoch{epoch:03d}",
@@ -82,6 +57,33 @@ def main(cfg: DictConfig):
         callbacks=[checkpoint_cb],
         gradient_clip_val=cfg.model.train.get("clip_grad", None),
         gradient_clip_algorithm="norm" if "clip_grad" in cfg.model.train else None,
+        precision=cfg.machine.precision,
     )
+
+    with trainer.init_module():
+        model = DINOModel(
+            output_dim=cfg.model.architecture.output_dim,
+            use_bn_in_head=cfg.model.architecture.use_bn_in_head,
+            norm_last_layer=cfg.model.architecture.norm_last_layer,
+            model_name=cfg.model.architecture.model_name,
+            patch_size=cfg.model.architecture.patch_size,
+            drop_path_rate=cfg.model.architecture.drop_path_rate,
+            lr=cfg.model.train.lr,
+            min_lr=cfg.model.train.min_lr,
+            batch_size_per_gpu=cfg.model.train.batch_size_per_gpu,
+            warmup_epochs=cfg.model.train.warmup_epochs,
+            weight_decay=cfg.model.train.weight_decay,
+            weight_decay_end=cfg.model.train.weight_decay_end,
+            warmup_teacher_temp=cfg.model.train.warmup_teacher_temp,
+            teacher_temp=cfg.model.train.teacher_temp,
+            warmup_teacher_temp_epochs=cfg.model.train.warmup_teacher_temp_epochs,
+            student_temp=cfg.model.train.student_temp,
+            center_momentum=cfg.model.train.center_momentum,
+            local_crops_number=cfg.model.transform.local_crops_number,
+            momentum_teacher=cfg.model.train.momentum_teacher,
+            world_size=cfg.machine.num_gpus * cfg.machine.num_nodes,
+            n_epochs=cfg.model.train.n_epochs,
+            n_dataloader_steps=len(dataloader),
+        )
 
     trainer.fit(model, dataloader)
