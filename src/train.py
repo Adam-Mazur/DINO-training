379a8805beeda82304fe1654b7f4bba2f0912492
Adam Mazur <adammazur445@gmail.com>
2026-01-28 14:20:04 +0100

    feat(train): Add an option to resume training from a checkpoint

--- src/train.py
+++ src/train.py
@@ -58,6 +58,7 @@ def main(cfg: DictConfig):
         gradient_clip_val=cfg.model.train.get("clip_grad", None),
         gradient_clip_algorithm="norm" if "clip_grad" in cfg.model.train else None,
         precision=cfg.machine.precision,
+        ckpt_path=cfg.paths.resume_from_checkpoint,
     )
 
     with trainer.init_module():
