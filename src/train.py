75e966ac69e8ecbe80e2b580787c40620fc1a57b
Adam Mazur <adammazur445@gmail.com>
2026-01-28 10:59:23 +0100

    feat(config): Copy the config for ResNet

--- src/train.py
+++ src/train.py
@@ -24,40 +24,40 @@ def main(cfg: DictConfig):
     run_ckpt_dir.mkdir(parents=True, exist_ok=True)
 
     transform = DataAugmentationDINO(
-        cfg.transform.global_crops_scale,
-        cfg.transform.local_crops_scale,
-        cfg.transform.local_crops_number,
+        cfg.model.transform.global_crops_scale,
+        cfg.model.transform.local_crops_scale,
+        cfg.model.transform.local_crops_number,
     )
 
     dataset = ImageFolder(cfg.paths.data_path, transform=transform)
     dataloader = DataLoader(
         dataset,
-        batch_size=cfg.train.batch_size_per_gpu,
+        batch_size=cfg.model.train.batch_size_per_gpu,
         shuffle=True,
         num_workers=cfg.machine.num_workers,
         drop_last=True,
     )
 
     model = DINOModel(
-        output_dim=cfg.model.output_dim,
-        use_bn_in_head=cfg.model.use_bn_in_head,
-        norm_last_layer=cfg.model.norm_last_layer,
-        model_name=cfg.model.model_name,
-        lr=cfg.train.lr,
-        min_lr=cfg.train.min_lr,
-        batch_size_per_gpu=cfg.train.batch_size_per_gpu,
-        warmup_epochs=cfg.train.warmup_epochs,
-        weight_decay=cfg.train.weight_decay,
-        weight_decay_end=cfg.train.weight_decay_end,
-        warmup_teacher_temp=cfg.train.warmup_teacher_temp,
-        teacher_temp=cfg.train.teacher_temp,
-        warmup_teacher_temp_epochs=cfg.train.warmup_teacher_temp_epochs,
-        student_temp=cfg.train.student_temp,
-        center_momentum=cfg.train.center_momentum,
-        local_crops_number=cfg.transform.local_crops_number,
-        momentum_teacher=cfg.train.momentum_teacher,
+        output_dim=cfg.model.architecture.output_dim,
+        use_bn_in_head=cfg.model.architecture.use_bn_in_head,
+        norm_last_layer=cfg.model.architecture.norm_last_layer,
+        model_name=cfg.model.architecture.model_name,
+        lr=cfg.model.train.lr,
+        min_lr=cfg.model.train.min_lr,
+        batch_size_per_gpu=cfg.model.train.batch_size_per_gpu,
+        warmup_epochs=cfg.model.train.warmup_epochs,
+        weight_decay=cfg.model.train.weight_decay,
+        weight_decay_end=cfg.model.train.weight_decay_end,
+        warmup_teacher_temp=cfg.model.train.warmup_teacher_temp,
+        teacher_temp=cfg.model.train.teacher_temp,
+        warmup_teacher_temp_epochs=cfg.model.train.warmup_teacher_temp_epochs,
+        student_temp=cfg.model.train.student_temp,
+        center_momentum=cfg.model.train.center_momentum,
+        local_crops_number=cfg.model.transform.local_crops_number,
+        momentum_teacher=cfg.model.train.momentum_teacher,
         world_size=cfg.machine.num_gpus * cfg.machine.num_nodes,
-        n_epochs=cfg.train.n_epochs,
+        n_epochs=cfg.model.train.n_epochs,
         n_dataloader_steps=len(dataloader),
     )
 
@@ -71,7 +71,7 @@ def main(cfg: DictConfig):
     )
 
     trainer = pl.Trainer(
-        max_epochs=cfg.train.n_epochs,
+        max_epochs=cfg.model.train.n_epochs,
         accelerator="gpu",
         devices=cfg.machine.num_gpus,
         num_nodes=cfg.machine.num_nodes,
